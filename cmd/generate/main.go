0077e0cf4787ecc96513330cc000491c540ea567
Hisam Fahri <me@hisamafahri.com>
2022-01-24 12:41:00 +0700

    feat(generate): add alias

--- cmd/generate/main.go
+++ cmd/generate/main.go
@@ -7,10 +7,11 @@ import (
 var Total uint
 
 var Generate = &cobra.Command{
-	Use:   "generate",
-	Short: "Generate various code, id, dummy data, etc",
-	Long:  `generate various amount and/or type of code, id, dummy data, etc`,
-	Args:  cobra.MinimumNArgs(1),
+	Use:     "generate",
+	Aliases: []string{"g"},
+	Short:   "Generate various code, id, dummy data, etc",
+	Long:    `generate various amount and/or type of code, id, dummy data, etc`,
+	Args:    cobra.MinimumNArgs(1),
 }
 
 func init() {
