a249cc1c2bfedc667e53eda14e6b0a3cb7adb55f
Hisam Fahri <me@hisamafahri.com>
2022-01-24 17:36:06 +0700

    fix(version): fix version test

--- cmd/root/version.go
+++ cmd/root/version.go
@@ -12,7 +12,7 @@ func VersionCmd() *cobra.Command {
 		Short: "Print the version number of Ting",
 		Long:  `printing the current version of ting CLI`,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			fmt.Println("ting v0.1.0")
+			fmt.Fprintf(cmd.OutOrStdout(), "ting v0.1.0\n")
 			return nil
 		},
 	}
--- cmd/root/version_test.go
+++ cmd/root/version_test.go
@@ -1,6 +1,8 @@
 package root
 
 import (
+	"bytes"
+	"io/ioutil"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -8,12 +10,15 @@ import (
 
 func TestVersion(t *testing.T) {
 	Convey("Calling the `ting version` command", t, func() {
+		cmd := VersionCmd()
+		b := bytes.NewBufferString("")
+		cmd.SetOut(b)
+		cmd.Execute()
+		out, _ := ioutil.ReadAll(b)
+		expected := "ting v0.1.0\n"
 
-		err := VersionCmd().Execute()
-
-		Convey("If no error", func() {
-			So(err, ShouldEqual, nil)
+		Convey("The command should return correct version", func() {
+			So(string(out), ShouldEqual, expected)
 		})
-
 	})
 }
